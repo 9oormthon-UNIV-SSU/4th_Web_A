9fda4763b3dfb8507a79e1f19849593a9ddbd00e
psubin05 <psubin0512@gmail.com>
2025-05-26 19:11:00 +0900

    7주차 실습 제출

---
+++ week7/src/movieDummy.jsx
@@ -0,0 +1,239 @@
+export const dummy = 
+{
+  "dates": {
+    "maximum": "2025-05-28",
+    "minimum": "2025-04-16"
+  },
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/2Nti3gYAX513wvhp8IiLL6ZDyOm.jpg",
+      "genre_ids": [
+        10751,
+        35,
+        12,
+        14
+      ],
+      "id": 950387,
+      "original_language": "en",
+      "original_title": "A Minecraft Movie",
+      "overview": "왕년의 게임 챔피언이었지만 지금은 폐업 직전의 게임샵 주인이 된 '개릿'과 엄마를 잃고 낯선 동네로 이사 온 남매 '헨리'와 '나탈리' 그리고 그들을 돕는 부동산 중개업자 '던'. 이들은 ‘개릿’이 수집한 ‘큐브’가 내뿜는 신비한 빛을 따라가다 어느 폐광 속에 열린 포털을 통해 미지의 공간으로 빨려들어간다. 산과 나무, 구름과 달, 심지어 꿀벌까지 상상하는 모든 것이 네모난 현실이 되는 이곳은 바로 ‘오버월드’. 일찍이 이 세계로 넘어와 완벽하게 적응한 ‘스티브’를 만난 네 사람은 지하세계 ‘네더’를 다스리는 마법사 ‘말고샤’의 침공으로 ‘오버월드’가 위험에 빠졌다는 사실을 알게 된다. 현실 세계로 돌아가기 위해서는 일단 살아남아야 하는 법! 다섯 명의 ‘동글이’들은 ‘오버월드’를 구하기 위해 힘을 합치게 되는데…",
+      "popularity": 694.9673,
+      "poster_path": "/k5aQ2TqKcQFwPoXHkpAGoKNVDLZ.jpg",
+      "release_date": "2025-04-26",
+      "title": "A MINECRAFT MOVIE 마인크래프트 무비",
+      "video": false,
+      "vote_average": 6.518,
+      "vote_count": 1436
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/uIpJPDNFoeX0TVml9smPrs9KUVx.jpg",
+      "genre_ids": [
+        27,
+        9648
+      ],
+      "id": 574475,
+      "original_language": "en",
+      "original_title": "Final Destination Bloodlines",
+      "overview": "끝없는 악몽에 시달리던 ‘스테파니’는 그 원인을 찾던 중  집안 대대로 내려오는 죽음의 저주와 그 실체를 알게 된다  놀랍게도, 죽음의 순서가 자신의 가족에게 이어졌음을 알게 되는데...  이제, 다음 차례는 누구?",
+      "popularity": 535.0346,
+      "poster_path": "/agaIglFrRGuUQOzkhzlrvcjJgt2.jpg",
+      "release_date": "2025-05-14",
+      "title": "파이널 데스티네이션: 블러드라인",
+      "video": false,
+      "vote_average": 7.061,
+      "vote_count": 408
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/7Zx3wDG5bBtcfk8lcnCWDOLM4Y4.jpg",
+      "genre_ids": [
+        10751,
+        35,
+        878
+      ],
+      "id": 552524,
+      "original_language": "en",
+      "original_title": "Lilo & Stitch",
+      "overview": "보송보송한 파란 솜털, 호기심 가득한 큰 눈, 장난기 가득한 웃음을 가졌지만..! 가장 위험한 실험체 취급을 받던 ‘스티치’는 우주에서 도망쳐 지구의 하와이 섬에 불시착하게 된다. 단짝 친구를 원하던 외톨이 소녀 ‘릴로’는 별똥별과 함께 나타난 귀여운 파란색 강아지(?) ‘스티치’와 소중한 친구이자, 하나의 가족이 되어가며 외로웠던 일상이 유쾌하게 변하기 시작한다. 그러던 어느 날, ‘스티치’를 잡아 우주로 되돌아가려는 정체불명의 요원들이 등장하고 ‘릴로’와 ‘스티치’는 예상치 못한 상황을 마주하게 되는데..!",
+      "popularity": 508.3123,
+      "poster_path": "/ww7jn7lv1YzTAGd5m0R6CP1VXAs.jpg",
+      "release_date": "2025-05-21",
+      "title": "릴로 & 스티치",
+      "video": false,
+      "vote_average": 7.116,
+      "vote_count": 160
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/1p5aI299YBnqrEEvVGJERk2MXXb.jpg",
+      "genre_ids": [
+        28,
+        12,
+        53
+      ],
+      "id": 575265,
+      "original_language": "en",
+      "original_title": "Mission: Impossible - The Final Reckoning",
+      "overview": "디지털상의 모든 정보를 통제할 수 있는 사상 초유의 무기로 인해 전 세계 국가와 조직의 기능이 마비되고, 인류 전체가 위협받는 절체절명의 위기가 찾아온다. 이를 막을 수 있는 건 오직 존재 자체가 기밀인 에단 헌트와 그가 소속된 IMF뿐이다. 무기를 무력화하는 데 반드시 필요한 키를 손에 쥔 에단 헌트. 오랜 동료 루터와 벤지, 그리고 새로운 팀원이 된 그레이스, 파리, 드가와 함께 지금껏 경험했던 그 어떤 상대보다도 강력한 적에 맞서 모두의 운명을 건 불가능한 미션에 뛰어든다.",
+      "popularity": 325.1914,
+      "poster_path": "/5Zxdorl5TharlI9S47YxoKzGCsi.jpg",
+      "release_date": "2025-05-17",
+      "title": "미션 임파서블: 파이널 레코닝",
+      "video": false,
+      "vote_average": 6.985,
+      "vote_count": 300
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/rthMuZfFv4fqEU4JVbgSW9wQ8rs.jpg",
+      "genre_ids": [
+        28,
+        878,
+        12
+      ],
+      "id": 986056,
+      "original_language": "en",
+      "original_title": "Thunderbolts*",
+      "overview": "어벤져스가 사라진 후, 세계 최대의 위협과 마주한 세상을 구하기 위해 전직 스파이, 암살자, 살인 청부 업자 등 마블의 별난 놈들이 펼치는 예측불허 팀플레이를 담은 액션 블록버스터",
+      "popularity": 167.4446,
+      "poster_path": "/1lI4iDsaVnOnnZPI9igWkL8jHPL.jpg",
+      "release_date": "2025-04-30",
+      "title": "썬더볼츠*",
+      "video": false,
+      "vote_average": 7.5,
+      "vote_count": 985
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/nAxGnGHOsfzufThz20zgmRwKur3.jpg",
+      "genre_ids": [
+        27,
+        53
+      ],
+      "id": 1233413,
+      "original_language": "en",
+      "original_title": "Sinners",
+      "overview": "1932년, 시카고 갱단의 생활을 정리하고 고향 미시시피로 돌아온 쌍둥이 형제 스모크와 스택은 큰돈을 벌기 위해 술집 주크 조인트를 운영하기로 한다. 화려한 오프닝 파티가 열리는 밤, 천부적인 재능을 지닌 새미의 노래로 파티의 열기는 점점 뜨거워지고 초대하지 않은 불청객 일행이 찾아오는데…",
+      "popularity": 111.2267,
+      "poster_path": "/5Tj7729s3MLmzNnAzaHSCWtzj5o.jpg",
+      "release_date": "2025-05-28",
+      "title": "씨너스: 죄인들",
+      "video": false,
+      "vote_average": 7.6,
+      "vote_count": 770
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/hwlyY7LJdEFbCPaGNXiskKKmJ5X.jpg",
+      "genre_ids": [
+        28,
+        53
+      ],
+      "id": 1126166,
+      "original_language": "en",
+      "original_title": "Flight Risk",
+      "overview": "범죄와 연루된 중요한 증인 ‘윈스턴’을 찾은 보안관 ‘해리스’는, 알래스카에서 뉴욕까지 ‘윈스턴’을 이송해야만 한다.  하지만 어딘가 수상한 비행기의 파일럿이 증인을 제거하려는 암살자임을 알게 되고, 이제 세상에서 가장 위험한 비행이 시작되는데… 도망갈 곳 없는 상공 1만 피트! 암살자를 피해 무사히 착륙해야 한다!",
+      "popularity": 58.6218,
+      "poster_path": "/zstC9sgsPaV98TaZtHL6aTtBUtB.jpg",
+      "release_date": "2025-05-20",
+      "title": "플라이트 리스크",
+      "video": false,
+      "vote_average": 6.1,
+      "vote_count": 743
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/n4R5Ad6rwf87lmllFNuYR3io30V.jpg",
+      "genre_ids": [
+        9648,
+        53
+      ],
+      "id": 1249213,
+      "original_language": "en",
+      "original_title": "Drop",
+      "overview": "몇 년 만의 데이트에 나선 싱글맘 바이올렛. 데이트 상대인 헨리와 즐거운 식사를 하던 도중 같은 레스토랑에 있는 누군가로부터 의문의 메시지를 받게 된다. 단순한 장난으로 치부하며 넘기려는 그녀에게 헨리를 죽이지 않으면 자신의 아들이 죽는다는 협박이 이어지고 이내 공포에 빠진 그녀는 익명의 위협에서 벗어나기 위해 고군분투하는데...",
+      "popularity": 40.3756,
+      "poster_path": "/l7Xj2XB7tRg1UqF4x5Prlc2LssQ.jpg",
+      "release_date": "2025-04-23",
+      "title": "드롭",
+      "video": false,
+      "vote_average": 6.524,
+      "vote_count": 347
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/jl2YIADk391yc6Qjy9JhgCRkHJk.jpg",
+      "genre_ids": [
+        16,
+        18,
+        35
+      ],
+      "id": 1064486,
+      "original_language": "en",
+      "original_title": "Memoir of a Snail",
+      "overview": "'그레이스'는 자신의 달팽이, '실비아'에게 살아온 이야기를 시작한다. 잦은 병치레, 수면무호흡증으로 언제 위험해질지 모르는 알코올 중독자 아빠, 또래 친구들의 괴롭힘… 씁쓸한 유년 시절 유일한 빛이자 영웅이 되어준 건 쌍둥이 '길버트'이다. 하지만 서로 다른 지역으로 입양돼 헤어지게 되며 서로에 대한 그리움으로 하루하루를 살아나간다. 계속되는 잔잔하고도 외로운 일상 속, 우연히 ‘핑키’라는 괴짜 할머니를 만나 우정을 쌓게 되고 그레이스는 다시금 인생의 희망을 찾아가기 시작하는데…",
+      "popularity": 35.7685,
+      "poster_path": "/vh549pxJL5GcQ8MN1jLgubOsNrq.jpg",
+      "release_date": "2025-04-30",
+      "title": "달팽이의 회고록",
+      "video": false,
+      "vote_average": 8.1,
+      "vote_count": 511
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/hRn5oyuhrVFjLwDpe73UvqgvPht.jpg",
+      "genre_ids": [
+        28,
+        14,
+        27
+      ],
+      "id": 793387,
+      "original_language": "ko",
+      "original_title": "거룩한 밤: 데몬 헌터스",
+      "overview": "악을 숭배하는 집단에 의해 혼란에 빠진 도시, 특별한 능력을 가진 어둠의 해결사 거룩한 밤 팀 바우, 샤론, 김군이 악의 무리를 처단하는 오컬트 액션",
+      "popularity": 28.0202,
+      "poster_path": "/7MGnHpfhF8savUsrUJoI8Q7dTk9.jpg",
+      "release_date": "2025-04-30",
+      "title": "거룩한 밤: 데몬 헌터스",
+      "video": false,
+      "vote_average": 5.9,
+      "vote_count": 4
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/aGgr2Q07AcyiWPGODoofgZDUEHp.jpg",
+      "genre_ids": [
+        28,
+        35,
+        10749
+      ],
+      "id": 1226406,
+      "original_language": "en",
+      "original_title": "Love Hurts",
+      "overview": "어두운 과거를 뒤로 한 채 성공한 부동산 중개인으로 소소한 행복을 찾아가며 살고 있는 마빈. 어느 날, 과거 함께 일했던 로즈의 예상치 못한 메시지를 받게 되고 자신이 몸 담았던 조직의 두목이자 그의 형인 너클스까지 그를 쫓기 시작한다. 평온한 삶을 되찾고 싶은 마빈은 결국  그토록 지우고 싶었던, 끝나지 않은 과거를 다시 한번 마주해야 하는데…",
+      "popularity": 22.2843,
+      "poster_path": "/fKYk2JsghymzIXRyVlfnVYH1b8U.jpg",
+      "release_date": "2025-05-14",
+      "title": "위드 러브",
+      "video": false,
+      "vote_average": 5.942,
+      "vote_count": 190
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kXxriYfAWtMJNHGHVYr09c37L07.jpg",
+      "genre_ids": [
+        878,
+        53
+      ],
+      "id": 650033,
+      "original_language": "ru",
+      "original_title": "Свободное падение",
+      "overview": "인류 역사상 최악의 태양 폭발로 순식간에 동료들을 잃은 ‘막심’. 숨 돌릴 틈도 없이 이어지는 폭발에 ‘막심’은 우주정거장과의 통신이 두절된 채 홀로 고"
+    }
+  ]
+}
---
+++ week7/src/pages/Movies.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Movie from '../components/Movie';
+import { dummy } from "../movieDummy";
+
+export default function Movies() {
+
+  return (
+    <div>
+      <div className="movies-container">
+        {dummy.results.map((item) => {
+          return (
+            <Movie
+              key={item.id}
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
