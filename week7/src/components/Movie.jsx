df4daade980de4f3e323e056e8269809d1a51b0d
psubin05 <psubin0512@gmail.com>
2025-05-27 16:13:35 +0900

    7주차 env 실습포함 다시 제출

--- week7/src/components/Movie.jsx
+++ week7/src/components/Movie.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-export const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
 
 export default function Movie(props) {
     const navigate = useNavigate();
@@ -9,12 +8,14 @@ export default function Movie(props) {
     const onClickMovieItem = () => {
         navigate(`/movie/${props.title}`, {
             state: props,
-        });
+        });  
     };
 
     return (
         <div className="movie-container" onClick={onClickMovieItem}>
-            <img src={IMG_BASE_URL + props.poster_path} alt="영화포스터" />
+            <img 
+                src={`${import.meta.env.VITE_IMG_BASE_URL}/${props.poster_path}`}
+                alt="영화포스터" />
             <div className="movie-info">
                 <h4>{props.title}</h4>
                 <span>{props.vote_average}</span>
