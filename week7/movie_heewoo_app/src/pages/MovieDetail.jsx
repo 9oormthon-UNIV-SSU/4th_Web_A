d7df6e79caef7133dc2f1200e65eed780658a1ef
hee4040 <hwoochoi7@gmail.com>
2025-05-27 16:12:42 +0900

    7주차 과제

--- week7/movie_heewoo_app/src/pages/MovieDetail.jsx
+++ week7/movie_heewoo_app/src/pages/MovieDetail.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
-import { IMG_BASE_URL } from "../components/Movie";
+// import { IMG_BASE_URL } from "../components/Movie";
 
 export default function MovieDetail(){
-    const {title} = useParams();
+    const {title} = useParams();    // url경로에 포함된 동적 파라미터 가져온다.
     const {state} = useLocation();
+
+    console.log("env", import.meta.env)
 
     return(
         <div className="page-container">
             <div style={{display:"flex"}}>
                 <img 
                     style={{width:"300px", height:"450px"}}
-                    src={IMG_BASE_URL + state.poster_path}
+                    src = {`${import.meta.env.VITE_IMG_BASE_URL}${state.poster_path}`}
                     alt="영화 포스터 이미지"
                 />
                 <div>
